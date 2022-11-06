3f8c079eb00d9033fe648656f55a8bdf072f9297
ninthsun91 <ninthsun91@gmail.com>
2022-11-06 21:48:44 +0900

    hotfix: no db while using local

--- __test__/app.spec.ts
+++ __test__/app.spec.ts
@@ -4,25 +4,24 @@ import env from '../src/config.env';
 import sequelize from '../src/db/config/connection';
 import association from '../src/db/config/associate';
 
-
 describe('app module test', () => {
     const OLD_ENV = process.env;
 
-    beforeAll(async()=>{
-        console.log(env);
-        await sequelize.authenticate();
-        association();
-    });
+    // beforeAll(async()=>{
+    //     console.log(env);
+    //     await sequelize.authenticate();
+    //     association();
+    // });
 
     beforeEach(()=>{
         jest.resetModules();
         process.env = { ...OLD_ENV };
     });
 
-    afterAll(async()=>{
-        process.env = OLD_ENV;
-        await sequelize.close();
-    });
+    // afterAll(async()=>{
+    //     process.env = OLD_ENV;
+    //     await sequelize.close();
+    // });
 
     test('should return status 200, if success', async() => {
         const response = await request(app).get('/');
