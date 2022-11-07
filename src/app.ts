ae280531b3a0b6c667895ad35d4241a2f2eab39e
ninthsun91 <ninthsun91@gmail.com>
2022-11-07 10:27:59 +0900

    set up view engine

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import env from './config.env';
 import express from 'express';
+import path from 'path';
+import ejs from 'ejs';
 import sequelize from './db/config/connection';
 import association from './db/config/associate';
 import cookieParser from 'cookie-parser';
@@ -23,12 +25,19 @@ if (env.NODE_ENV !== 'test') {
     });    
 }
 
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 app.use(express.json());
 
 app.use(Router);
 
 app.use(error.logger, error.handler);
+
+
 
 
 export default app;
--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,18 @@
 import { Router } from 'express';
 
 import UserRouter from './user/user.routes';
+import PageRouter from './page.routes';
 
 
 const router = Router();
 
-router.get('/', async(req, res, next) => {
+router.get('/test', async(req, res, next) => {
     res.status(200).json({
         message: 'INDEX PAGE',
     });
 });
 
 router.use('/user', UserRouter);
-
+router.use('/', PageRouter);
 
 export default router;
