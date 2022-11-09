3e69bcda3fa1f446ac8e2e08f2372610a7f25b64
ninthsun91 <ninthsun91@gmail.com>
2022-11-09 21:49:14 +0900

    merge test

--- server.ts
+++ server.ts
@@ -2,5 +2,6 @@ import httpServer from './src/app';
 import env from './src/config.env';
 
 httpServer.listen(env.PORT, () => {
+    console.log(env.NODE_ENV);
     console.log('SERVER RUNNING ON ', env.PORT);
 });
